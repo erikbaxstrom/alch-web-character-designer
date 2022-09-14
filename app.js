8faac202de566f3c6d23da78302ba1e1665e1142
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-14 15:18:18 -0700

    add phrase list functionality

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const character = {
     head: 'bird',
     middle: 'dress',
     pants: 'white',
-    phrases: ['blue', '123'],
+    phrases: [],
 };
 
 headSelect.addEventListener('change', () => {
@@ -61,9 +61,11 @@ function displayPhrases() {
 
 addButton.addEventListener('click', () => {
     // > Get the phrase from the value of the phrase input
+    const phrase = phraseInput.value;
     // > Use ".push" to add the phrase to the end of the character phrases array
+    character.phrases.push(phrase);
     // > Re-display the phrases
-
+    displayPhrases();
     // reset the input
     phraseInput.value = '';
 });
